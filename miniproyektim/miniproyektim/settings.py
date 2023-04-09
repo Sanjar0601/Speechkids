a36b171cdd05ad7a21d1c7031d89f10c4fb68829
Bobur21215 <nobleman041197@gmail.com>
2023-04-09 18:18:47 +0500

    assasa

--- miniproyektim/miniproyektim/settings.py
+++ miniproyektim/miniproyektim/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5ce4q9+&e5)b3lohbogst$phxt#&)!m6ga4h(9xstc7%4%b%&d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
